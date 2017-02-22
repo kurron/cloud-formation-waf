484b4f880a0884890883981bce14654b9b9eeac4
Ron Kurr <kurr@kurron.org>
2017-02-22 12:04:15 -0500

    Remove unused parameters

--- scripts/create-stack.sh
+++ scripts/create-stack.sh
@@ -3,9 +3,9 @@
 # creates a stack in AWS via CloudFromation
 
 STACKNAME=${1:-Weapon-X-WAF}
-PROJECTNAME=${2:-Weapon-X}
-ENVIRONMENT=${3:-development}
-CREATOR=${4:-CloudFormation}
+NAME=${2:-Example WAF}
+METRICNAME=${3:-ExampleWAF}
+PRIORITY=${4:-1}
 TEMPLATELOCATION=${5:-file://$(pwd)/waf.yml}
 
 VALIDATE="aws cloudformation validate-template --template-body $TEMPLATELOCATION"
@@ -15,9 +15,9 @@ $VALIDATE
 CREATE="aws cloudformation create-stack --stack-name $STACKNAME \
                                         --template-body $TEMPLATELOCATION \
                                         --capabilities CAPABILITY_NAMED_IAM \
-                                        --parameters ParameterKey=Project,ParameterValue=$PROJECTNAME \
-                                                     ParameterKey=Creator,ParameterValue=$CREATOR \
-                                                     ParameterKey=Environment,ParameterValue=$ENVIRONMENT \
+                                        --parameters ParameterKey=Name,ParameterValue=$NAME \
+                                                     ParameterKey=MetricName,ParameterValue=$METRICNAME \
+                                                     ParameterKey=Priority,ParameterValue=$PRIORITY \
                                         --tags Key=Project,Value=$PROJECTNAME \
                                                Key=Environment,Value=$ENVIRONMENT \
                                                Key=Creator,Value=$CREATOR"
